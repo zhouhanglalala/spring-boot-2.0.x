0fee46f24aa688f80aff4a27dc5e3b885cd04645
zhouhang <zhouhanglalala@163.com>
2021-08-15 12:59:42 +0800

    初始化

---
+++ spring-boot-project/spring-zhouhang-boot/src/main/java/com/zhouhang/demo/SpringbootDemoApplication.java
@@ -0,0 +1,17 @@
+package com.zhouhang.demo;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ * @author zhouhang
+// */ //
+@SpringBootApplication
+public class SpringbootDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootDemoApplication.class, args);
+    }
+
+}
+            
